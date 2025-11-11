92866f1e0c82ea3dd082a0e69fde34b7775cd7c3
dawidpiechaczek <dawidpiechaczek.20@gmail.com>
2025-11-11 09:18:46 +0100

    POTATO-5: Make libs available in build-logic directory.

--- build-logic/build.gradle.kts
+++ build-logic/build.gradle.kts
@@ -17,9 +17,6 @@ gradlePlugin {
 }
 
 dependencies {
-    // Dependency for the Android Gradle Plugin API (`com.android.library`)
-    implementation("com.android.tools.build:gradle:8.1.2") // Use the AGP version you use in your project
-
-    // Dependency for the Kotlin Multiplatform Gradle Plugin API (`org.jetbrains.kotlin.multiplatform`)
-    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.20") // Use the Kotlin version you use in your project
+    implementation(libs.gradle)
+    implementation(libs.kotlin.gradle.plugin)
 }
